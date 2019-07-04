5b62af79481e4123fecb83c4e696f660f6b59385
Scabandari <ryan3nichols@gmail.com>
2019-07-04 13:35:40 -0400

    Just cleaning up some console.logs

--- utils.js
+++ utils.js
@@ -3,11 +3,8 @@ const fs = require('fs');
 const Game = require('./models/game');
 
 exports.populate_database = (jsonFile) => {
-    //console.log("Working");
     const games_list = fs.readFileSync(jsonFile);
     const games = JSON.parse(games_list);
-    // console.log(`games length: ${games.length}`);
-    // console.log(`games[0]: ${JSON.stringify(games[0], null, 2)}`);
     games.forEach(async game => {
         const game_ = new Game(game);
         await game_.save();
@@ -27,9 +24,7 @@ exports.words_in_sentence = (word_list, sentence) => {
 
 exports.id_in_list = (id, list) => {
   // returns true if an object with that id is already in the list
-    //console.log(`id: ${id}`);
     list.forEach(list_item => {
-      //console.log(`list_item._id: ${list_item._id}`);
       if(list_item._id === id) {
           return true;
       }
